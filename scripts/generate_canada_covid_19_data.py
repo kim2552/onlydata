703923386f4f7bca2cde49e49273aaea8036dcb4
joohoonkim <davidkim2552@gmail.com>
2021-06-02 23:52:26 -0400

    cleaned up unused functions

--- scripts/generate_canada_covid_19_data.py
+++ scripts/generate_canada_covid_19_data.py
@@ -1,15 +1,20 @@
 import csv
 from urllib.request import urlopen
 import codecs
-import datetime
 import json
 
+CSV_VAC_URL = "https://health-infobase.canada.ca/src/data/covidLive/vaccination-administration.csv"
 CSV_URL = "https://health-infobase.canada.ca/src/data/covidLive/covid19-download.csv"
 FILE_PATH = "src/assets/"
 
 selection_list = [ "Canada", "British Columbia", "Alberta", "Saskatchewan", "Manitoba", "Ontario", "Quebec", "Newfoundland and Labrador", "New Brunswick", "Nova Scotia", "Prince Edward Island", "Yukon", "Northwest Territories", "Nunavut"]
 
-def create_selection_covid19_data_per_day(selection="Canada"):
+'''
+Creates a csv file that contains a table for the selected region and the selected category (header) for a list of dates
+selection: A Province or Territory of Canada or Canada
+category: available categories are numtoday, numtotal, numconf, numdeath, numactive
+'''
+def create_selection_covid19_data_per_day(selection="Canada", category="numtoday"):
     response = urlopen(CSV_URL)
     csv_reader = csv.reader(codecs.iterdecode(response, 'utf-8'), delimiter=',')
     f = open(FILE_PATH+selection.lower()+'-covid-19-data-per-day.csv','w', newline='')
@@ -17,96 +22,30 @@ def create_selection_covid19_data_per_day(selection="Canada"):
     line_count = 0
     desired_row = 0
     
-    yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
-    yesterdays_date = yesterday.strftime('%Y-%m-%d')
-    update_date(yesterdays_date)
+    most_recent_date = "0"    # Gets the most recent date in the data
     
     for row in csv_reader:
         if line_count == 0:
             writer.writerow(['Date','Number of Confirmed Cases'])
             line_count += 1
             for index,header in enumerate(row):
-                if(header == "numtoday"):
+                if(header == category):
                     desired_row = index
         elif( row[1].lower() == selection.lower() ):
-            writer.writerow([row[3], row[desired_row]])
+            writer.writerow([row[3], row[desired_row]]) # date, catagory value
+            if(row[3] > most_recent_date):
+                print(row[3], most_recent_date, row[3] > most_recent_date)
+                most_recent_date = row[3]
             line_count += 1
+
+    if(most_recent_date != "0"):               # Update the posts file with the most recent dated update
+        update_date(most_recent_date)
+
     f.close()
 
-def create_canada_covid19_data_total():
-    response = urlopen(CSV_URL)
-    csv_reader = csv.reader(codecs.iterdecode(response, 'utf-8'), delimiter=',')
-    f = open(FILE_PATH+'canada-covid-19-data-total.csv','w', newline='')
-    writer = csv.writer(f)
-    line_count = 0
-    desired_row = 0
-
-    yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
-    yesterdays_date = yesterday.strftime('%Y-%m-%d')
-    update_date(yesterdays_date)
-
-    for row in csv_reader:
-        if line_count == 0:
-            writer.writerow(['Date','Number of Confirmed Cases'])
-            line_count += 1
-            for index,header in enumerate(row):
-                if(header == "numtotal"):
-                    desired_row = index
-        elif( row[1] == 'Canada' ):
-            writer.writerow([row[3], row[desired_row]])
-            line_count += 1
-    f.close()
-
-def create_canada_covid19_data_by_province():
-    response = urlopen(CSV_URL)
-    csv_reader = csv.reader(codecs.iterdecode(response, 'utf-8'), delimiter=',')
-    f = open(FILE_PATH+'province-covid-19-data-by-province.csv','w', newline='')
-    writer = csv.writer(f)
-
-    yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
-    yesterdays_date = yesterday.strftime('%Y-%m-%d')
-
-    line_count = 0
-    desired_row = 0
-    for row in csv_reader:
-        if line_count == 0:
-            writer.writerow(['Province/Territory','Number of Confirmed Cases'])
-            line_count += 1
-            for index,header in enumerate(row):
-                if(header == "numtotal"):
-                    desired_row = index
-        elif( row[3] == yesterdays_date ):
-            if( row[1] != "Canada" and row[1] != "Repatriated travellers"):
-                writer.writerow([row[1], row[desired_row]])
-                line_count += 1
-    f.close()
-
-def create_canada_covid19_active_data_by_province():
-    response = urlopen(CSV_URL)
-    csv_reader = csv.reader(codecs.iterdecode(response, 'utf-8'), delimiter=',')
-    f = open(FILE_PATH+'province-covid-19-active-data-by-province.csv','w', newline='')
-    writer = csv.writer(f)
-
-    yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
-    yesterdays_date = yesterday.strftime('%Y-%m-%d')
-    update_date(yesterdays_date)
-
-    line_count = 0
-    desired_row = 0
-
-    for row in csv_reader:
-        if line_count == 0:
-            writer.writerow(['Province/Territory','Number of Active Cases'])
-            line_count += 1
-            for index,header in enumerate(row):
-                if(header == "numactive"):
-                    desired_row = index
-        elif( row[3] == yesterdays_date ):
-            if( row[1] != "Canada" and row[1] != "Repatriated travellers"):
-                writer.writerow([row[1], row[desired_row]])
-                line_count += 1
-    f.close()
-
+'''
+Updates the posts.json file with the last updated date.
+'''
 def update_date(new_date):
     json_file = open(FILE_PATH+'posts.json', "r")
     data = json.load(json_file)
@@ -120,13 +59,7 @@ def update_date(new_date):
     new_json_file.close()
 
 def generate_canada_covid19_data():
-
     for selection in selection_list:
-        create_selection_covid19_data_per_day(selection)
-
-    # create_canada_covid19_data_total()
-    # create_canada_covid19_data_by_province()
-    # create_canada_covid19_active_data_by_province()
-
+        create_selection_covid19_data_per_day(selection,"numtoday")
 
 generate_canada_covid19_data()
